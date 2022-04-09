7bf050aaf448a10aa9a6b98bc20fc73cb228c0ec
sangphan45 <sieungansang000@gmail.com>
2022-04-09 23:31:06 +0700

    update

--- app/src/main/java/com/nhatsangthi/chatsapp/ChatActivity.java
+++ app/src/main/java/com/nhatsangthi/chatsapp/ChatActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.nhatsangthi.chatsapp.databinding.ActivityChatBinding;
+
 public class ChatActivity extends AppCompatActivity {
+    ActivityChatBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String name = getIntent().getStringExtra("name");
+        String uid = getIntent().getStringExtra("uid");
+
+        getSupportActionBar().setTitle(name);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
