9bd289218757c881cf048990fd390dbb11f5641f
sangphan45 <sieungansang000@gmail.com>
2022-04-09 22:33:00 +0700

    update.

--- app/src/main/java/com/nhatsangthi/chatsapp/OTPActivity.java
+++ app/src/main/java/com/nhatsangthi/chatsapp/OTPActivity.java
@@ -3,13 +3,21 @@ package com.nhatsangthi.chatsapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.mukesh.OnOtpCompletionListener;
 import com.nhatsangthi.chatsapp.databinding.ActivityOtpactivityBinding;
 
 import java.util.concurrent.TimeUnit;
@@ -18,13 +26,24 @@ public class OTPActivity extends AppCompatActivity {
     ActivityOtpactivityBinding binding;
     FirebaseAuth auth;
 
+    String verificationId;
+
+    ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityOtpactivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Sending OTP...");
+        dialog.setCancelable(false);
+        dialog.show();
+
         auth = FirebaseAuth.getInstance();
+
+        getSupportActionBar().hide();
 
         String phoneNumber = getIntent().getStringExtra("phoneNumber");
 
@@ -46,11 +65,34 @@ public class OTPActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                        super.onCodeSent(s, forceResendingToken);
+                    public void onCodeSent(@NonNull String verifyId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(verifyId, forceResendingToken);
+
+                        dialog.dismiss();
+                        verificationId = verifyId;
                     }
                 }).build();
 
         PhoneAuthProvider.verifyPhoneNumber(options);
+
+        binding.otpView.setOtpCompletionListener(new OnOtpCompletionListener() {
+            @Override
+            public void onOtpCompleted(String otp) {
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, otp);
+
+                auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(OTPActivity.this, SetupProfileActivity.class);
+                            startActivity(intent);
+                            finishAffinity();
+                        } else {
+                            Toast.makeText(OTPActivity.this, "Failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
